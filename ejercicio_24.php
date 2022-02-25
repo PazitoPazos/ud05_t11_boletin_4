37b70a5ca7f4b9b3129ed91fe3e0ad9dccf08781
PazitoPazos <javier.pazos001@gmail.com>
2022-02-25 09:22:24 +0100

    Ejercicio 24

---
+++ ejercicio_24.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ejercicio 24</title>
+    </head>
+    <body>
+        <h3>Ejercicio 24</h3>
+        <p>Escribe un programa que lea un número N e imprima una pirámide de números con N filas como en la siguiente figura. Recuerda utilizar un tipo de letra de ancho fijo como por ejemplo Courier para que los espacios tengan la misma anchura que los números.</p>
+
+        <form action="ejercicio_24.php" method="post">
+            <p><input type="text" name="num"></p>
+            <p><input type="submit" value="Calcular"></p>
+        </form>
+
+        <?php
+        $h=isset($_REQUEST["num"])&&is_numeric($_REQUEST["num"])?intval($_REQUEST["num"]):"a";
+        $fill=$_REQUEST["fill"];
+            for ($i=0; $i < $h; $i++) { 
+                for ($j=0; $j <= $i; $j++) { 
+                    echo "a&nbsp;";
+                }
+                echo "<br />";
+            }
+        ?>
+    </body>
+</html>
